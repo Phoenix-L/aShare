41590df8c5087ca8c46e21cfdbe97ef5a07b60c7
Phoenix-L <lianghaitian@hotmail.com>
2026-03-17 21:52:47 +0800

    phase2: unify grid search implementation (#23)

--- src/ashare/experiment/__init__.py
+++ src/ashare/experiment/__init__.py
@@ -1,7 +1,7 @@
 """Experiment specification and grid utilities."""
 
-from ashare.experiment.grid import generate_parameter_sets
+from ashare.experiment.grid import expand_grid, generate_parameter_sets
 from ashare.experiment.result import build_summary, collect_run_results, rank_results
 from ashare.experiment.spec import load_experiment_spec
 
-__all__ = ["load_experiment_spec", "generate_parameter_sets", "collect_run_results", "rank_results", "build_summary"]
+__all__ = ["load_experiment_spec", "expand_grid", "generate_parameter_sets", "collect_run_results", "rank_results", "build_summary"]
